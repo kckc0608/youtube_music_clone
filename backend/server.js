601ed03030df5e93e91794aacbb5666ce49d8dc6
Neang_Chi <64959010+kckc0608@users.noreply.github.com>
2022-06-07 11:37:18 +0000

    test query

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,22 @@ const db = require('./db.js');
 const app = express();
 const port = 3000;
 
+const makeSongObj = function(song, singer, imageDir) {
+	const obj = {};
+	obj.song = song;
+	obj.singer = singer;
+	obj.image_dir = imageDir;
+
+	return obj;
+}
+
 app.listen(port, function(){
 	console.log('listening on 3000');
 });
 
 // GET == URL
 app.get('/', function(req, res) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.send({
+	resultObj = {
 		fastStartList: [{
 				song: "Re:Wind",
 				singer: "이세계 아이돌",
@@ -21,15 +29,21 @@ app.get('/', function(req, res) {
 				imageDir: "res/song/re_wind_album.jpeg"
 			}
 		]
-	});
-	//res.json
-	//res.send('hello world!');
-	//res.sendFile(__dirname + '/index.html');
+	};
 
 	const fast_start_query = 'select * from fast_start';
 
 	db.query(fast_start_query, (err, result) => {
 		console.log(err);
 		console.log(result);
+		if (err === null) {
+			resultObj.fastStartList.push(makeSongObj(result[0].song, result[0].singer, result[0].image_dir));
+		}
 	});
+
+	res.header("Access-Control-Allow-Origin", "*");
+	res.send(resultObj);
+	//res.json
+	//res.send('hello world!');
+	//res.sendFile(__dirname + '/index.html');
 });
