68316be1723db5a9f9de05afc34048d96ad2cd55
kwon <kckc0608@naver.com>
2022-08-14 19:59:16 +0900

    feature : 아이콘 버튼에 마우스를 가져다 대면 손모양 커서로 변경

--- frontend/script/button_event/main.js
+++ frontend/script/button_event/main.js
@@ -1,5 +1,19 @@
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function showAccountModal() {
+    
+}
+
 const account_button = document.querySelector('#account');
 account_button.addEventListener("click", () => {
-    location.href = "views/login"; // 'domain/login'
+    if(getCookie("auth") !== undefined)   
+        showAccountModal();
+    else
+        location.href = "views/login"; // 'domain/login'
 
 });
