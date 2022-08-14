c9c8a318d4b0a1256a8146ba0c9856fe06b1303b
kwon <kckc0608@naver.com>
2022-08-14 20:39:36 +0900

    feature : 로그인 성공시 계정 아이콘 클릭 이벤트 수정 (개요)
    로그인에 성공하였을 때 계정 아이콘을 클릭하면 현재는 똑같이 로그인
    페이지로 이동하고 있습니다.
    실제 유튜브 뮤직처럼 로그인한 계정의 간단한 정보를 모달창으로 확인
    하고, 계정 변경, 로그아웃, 계정 정보 수정 등의 기능을 할 수 있게
    만들려고 합니다..

--- frontend/script/button_event/main.js
+++ frontend/script/button_event/main.js
@@ -1,5 +1,7 @@
 const account_button = document.querySelector('#account');
 account_button.addEventListener("click", () => {
-    location.href = "views/login"; // 'domain/login'
+    //브라우저 단에서 쿠키 체크 후 쿠키가 있으면 로그인 된 것으로 간주
 
+    // 만약 쿠키가 없으면 로그인 페이지로 이동
+    location.href = "views/login";
 });
