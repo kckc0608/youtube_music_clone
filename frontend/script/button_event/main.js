090ffc69184aa64aa96609b0a30844198211c2e8
kwon <kckc0608@naver.com>
2022-08-15 19:37:51 +0900

    FEAT : 계정 버튼에 로그인 여부 확인 기능 추가
    쿠키를 여부를 확인하여 로그인 여부 확인
    쿠키 여부를 확인하기 위한 공통 함수도 작성

--- frontend/script/button_event/main.js
+++ frontend/script/button_event/main.js
@@ -1,7 +1,10 @@
+import { getCookie } from "../common/cookie.";
 const account_button = document.querySelector('#account');
 account_button.addEventListener("click", () => {
-    //브라우저 단에서 쿠키 체크 후 쿠키가 있으면 로그인 된 것으로 간주
-
-    // 만약 쿠키가 없으면 로그인 페이지로 이동
-    location.href = "views/login";
+    if (!!getCookie("auth")) {
+        alert("이미 로그인하였습니다.");
+    }
+    else {
+        location.href = "views/login";
+    }
 });
---
+++ frontend/script/common/cookie..js
@@ -0,0 +1,6 @@
+export const getCookie = function(key) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + key.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+};
