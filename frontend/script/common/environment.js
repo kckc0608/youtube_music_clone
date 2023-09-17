4c524ea85d561c0f0083833405b56d0af616f0b4
kckc0608 <kckc0608@gmail.com>
2023-09-17 20:08:31 +0900

    api address change

--- frontend/script/common/environment.js
+++ frontend/script/common/environment.js
@@ -1,2 +1,1 @@
-//export const API_ADDRESS = 'http://everdu.com/api/project/ym_clone/';
-export const API_ADDRESS = 'http://127.0.0.1:3000/api/project/ym_clone/';
+export const API_ADDRESS = 'http://everdu.com/api/project/ym_clone/';
